5ce08a74cf91f451fd17b6f2997b0e0f2edc8cf9
romaindemoustier <romain.demoustier@condorcet.be>
2014-09-29 14:35:51 +0200

    personnel

---
+++ pages/Liste_personnel.php
@@ -0,0 +1,9 @@
+<h2>Liste du personnel d'entretien</h2>
+<section>
+    <div>
+    <h3>Batiment F</h3>
+    <span>Julien</span>
+    <span>Rosalie</span>
+    <span>marco</span>
+    </div>
+</section>
